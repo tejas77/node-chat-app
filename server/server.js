a8a62c8cdf222d55b81e2b20a1c32ff3be1bc1ef
Tejas Rathod <happytejasrathod@gmail.com>
2019-04-27 17:15:03 +0530

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,14 +13,29 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User Connected');
-
-    socket.on('createMessage', (message) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: 'New user Joined',
             createAt: new Date().getTime()
         });
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
