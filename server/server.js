d4ebb206986be96c63d3c00ab2ef48cb8a929622
Tejas Rathod <happytejasrathod@gmail.com>
2019-05-03 04:25:52 +0530

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -39,12 +39,17 @@ io.on('connection', (socket) => {
         callback();
     });
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        var user = users.getUser(socket.id);
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));    
+        }
         callback();
     });
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+        var user = users.getUser(socket.id);
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude))
+        }
     });
     socket.on('disconnect', () => {
         var user = users.removeUser(socket.id);
@@ -58,6 +63,5 @@ io.on('connection', (socket) => {
 
 server.listen(port, () => {
     console.log('Started on port ', port);
-    
 });
 module.exports = {app};
